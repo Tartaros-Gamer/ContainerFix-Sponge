7319e3fc4c2c8140371fd7323984507c27089671
ahdg <36436808+ahdg6@users.noreply.github.com>
2022-08-24 23:22:37 +0800

    build it

---
+++ README.md
@@ -0,0 +1,11 @@
+### ContainerFix (Sponge API 7)
+
+A simple plugin to fix dupe glitch among mods by preventing GUI keep after block break and being multi-open. 
+
+一个防止容器多开和容器状态改变后 GUI 保留的 Sponge 插件，用于修复奇奇怪怪的 mod bug. 
+
+### Usage / 用法
+
+Configrate the targe block on `/config/containerfix.conf`
+
+在 `/config/containerfix.conf` 添加您要预防的方块
--- src/main/java/io/github/ahdg/containerfix/conf/Conf.java
+++ src/main/java/io/github/ahdg/containerfix/conf/Conf.java
@@ -6,28 +6,33 @@ import ninja.leaping.configurate.objectmapping.serialize.ConfigSerializable;
 @ConfigSerializable
 public class Conf {
 
-    @Setting(comment = "防止GUI保持的容器ID列表 \n" +
-    " \\\"minecraft:chest\\\" # 箱子\n" +
+    @Setting(comment = "The block that will be targeted when prevent GUI Keep.\n" +
+            "防止GUI保持的容器ID列表 \n" +
+    " \\\"minecraft:chest\\\" # 箱子(Chest)\n" +
     " \\\"minecraft:stone:3\" # 闪长岩(1.12)")
     public String[] AntiGUIKeepContainerList = {
             "minecraft:chest",
             "minecraft:stone:3"
     };
 
-    @Setting(comment = "防止多开的容器ID列表 \n" +
-            " \\\"minecraft:chest\\\" # 箱子\n" +
+    @Setting(comment = "The block that will be targeted when prevent multi-players from opening.\n"
+            + "防止多开的容器ID列表 \n" +
+            " \\\"minecraft:chest\\\" # 箱子(Chest)\n" +
             " \\\"minecraft:stone:3\" # 闪长岩(1.12)")
     public String[] AntiMultiOpenContainerList = {
             "minecraft:chest",
             "minecraft:stone:3"
     };
 
-    @Setting(comment = "在容器被打开时，破坏方块的行为，true 代表阻止方块破坏，false 代表强行关闭 GUI")
+    @Setting(comment = "Set If Prevent block from breaking when target block was occupied.\n"
+            + "在容器被打开时，破坏方块的行为，true 代表阻止方块破坏，false 代表强行关闭 GUI")
     public boolean PreventBreak = true;
 
-    @Setting(comment = "容器多开时，向玩家发送的信息")
+    @Setting(comment = "The message send to player who open a occupied block.\n"
+            + "容器多开时，向玩家发送的信息")
     public String MessagesMultiOpen = "该方块目前正在被其他玩家占用哦，请稍后再试";
 
-    @Setting(comment = "容器正在开启时，另一名玩家破坏方块时发送的信息")
+    @Setting(comment = "The message send to player who break a occupied block.\n"
+            + "容器正在开启时，另一名玩家破坏方块时发送的信息")
     public String MessagesAntiGUIKeep = "有人正在开这个容器啦，等别人用完再破坏这个方块吧";
 }
